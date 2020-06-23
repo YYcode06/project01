48c82ceb22044973c3557f019ccc65e18fd14498
yangying07 <yangying07@youxin.com>
2020-06-23 18:46:44 +0800

    fix登录问题

--- vue-element-admin-master/src/store/modules/user.js
+++ vue-element-admin-master/src/store/modules/user.js
@@ -32,6 +32,7 @@ const user = {
         login(username, userInfo.password).then(response => {
           const data = response.data
           setToken(data.token)
+          console.log(data.token, 'token')
           commit('SET_TOKEN', data.token)
           resolve()
         }).catch(error => {
--- vue-element-admin-master/src/views/login/index.vue
+++ vue-element-admin-master/src/views/login/index.vue
@@ -78,6 +78,7 @@ export default {
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
         if (valid) {
+          console.log(valid, this.loginForm)
           this.loading = true
           this.$store.dispatch('Login', this.loginForm).then(() => {
             this.loading = false
